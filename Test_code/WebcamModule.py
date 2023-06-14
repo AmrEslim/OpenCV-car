b59225f82571fc343114f5fe22ca705eeb1edae7
Amr Eslim <s0580554@htw-berlin.de>
2023-06-14 11:55:18 +0100

    updated WarpPoint position and window size

--- Test_code/WebcamModule.py
+++ Test_code/WebcamModule.py
@@ -1,12 +1,16 @@
 import cv2
- 
+
 cap = cv2.VideoCapture(cv2.CAP_V4L2)
- 
-def getImg(display= False,size=[480,240]):
+#cap = cv2.VideoCapture(-1)
+
+def getImg(display=False, size=[240, 180]):
     _, img = cap.read()
-    img = cv2.resize(img,(size[0],size[1]))
+    img = cv2.resize(img, (size[0], size[1]))
+    if display:
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
     return img
- 
+
 if __name__ == '__main__':
     while True:
         img = getImg(True)
--- Test_code/utlis.py
+++ Test_code/utlis.py
@@ -24,16 +24,16 @@ def nothing(a):
     pass
 
 
-def initializeTrackbars(intialTracbarVals, wT=480, hT=720):
+def initializeTrackbars(intialTracbarVals, wT=240, hT=180):
     cv2.namedWindow("Trackbars")
-    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.resizeWindow("Trackbars", 300, 180)
     cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0], wT // 2, nothing)
     cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
     cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2], wT // 2, nothing)
     cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
 
 
-def valTrackbars(wT=480, hT=240):
+def valTrackbars(wT=240, hT=180):
     widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
     heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
     widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
